11e4f80f07c2ac094cf167d76ce453d8aaacc781
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2019-12-19 23:03:04 -0800

    Solution to rotate image

---
+++ Arrays and Strings/1.7/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean rotateMatrix(int[][] matrix) {
+        //check if N x N matrix or not
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            return false;
+        }
+
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n/2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];     //save the top
+
+                //left --> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                //bottom --> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                //right --> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                //top --> right
+                matrix[i][last] = top;
+            }
+        }
+        return true;
+    }
+}
